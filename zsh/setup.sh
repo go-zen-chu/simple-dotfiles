927e0ac335ee3c782b3d518e78423593d1210d64
Akira Masuda <masuda.akira1014@gmail.com>
2019-09-22 18:49:54 +0900

    update

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -8,4 +8,4 @@ brew install zplug
 chsh -s /usr/local/bin/zsh || true # for skipping in CI
 
 touch "${HOME}/local.zsh"
-cp -i ./zsh/.zshrc "${HOME}"
+cp -i ./zsh/.zshrc "${HOME}" || true # for skipping in CI
